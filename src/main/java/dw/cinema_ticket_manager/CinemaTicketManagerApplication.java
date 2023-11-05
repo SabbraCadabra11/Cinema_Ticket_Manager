7ac10900b5e9c15ec091be1b41489422707df2a0
sabbrcadabr <dawid.want11@gmail.com>
2023-11-05 13:13:03 +0100

    Setting up new project

---
+++ src/main/java/dw/cinema_ticket_manager/CinemaTicketManagerApplication.java
@@ -0,0 +1,13 @@
+package dw.cinema_ticket_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dw/cinema_ticket_manager/CinemaTicketManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package dw.cinema_ticket_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
