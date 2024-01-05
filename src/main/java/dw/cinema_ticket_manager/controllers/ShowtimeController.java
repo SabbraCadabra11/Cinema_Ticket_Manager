2b2794b92d432372374414dab9823b8b6b6689ba
sabbr <dawid.want111@gmail.com>
2024-01-05 18:14:35 +0100

    /showtimes changes, works with Thymeleaf fragments

--- src/main/java/dw/cinema_ticket_manager/controllers/ShowtimeController.java
+++ src/main/java/dw/cinema_ticket_manager/controllers/ShowtimeController.java
@@ -6,10 +6,7 @@ import dw.cinema_ticket_manager.services.impl.ShowtimeServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.view.RedirectView;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.*;
@@ -28,23 +25,23 @@ public class ShowtimeController {
     }
 
     @GetMapping
-    public RedirectView showtimes(Model model) {
+    public String redirectToDefaultDate(Model model) {
         var allDates = getAllShowtimeDates();
         var firstShowtimeDate = allDates.get(0);
-        model.addAttribute("allDates", allDates);
-        model.addAttribute("showtimes", showtimeService.getShowtimesByDate(firstShowtimeDate));
-        return new RedirectView("/showtimes/" + firstShowtimeDate);
-    }
-
-    @GetMapping("/{date}")
-    public String showtimesByDate(@PathVariable("date") String date, Model model) {
-        validatePathVariable(date); // Validate the path variable here
-        var allDates = getAllShowtimeDates();
-        var showtimes = showtimeService.getShowtimesByDate(LocalDate.parse(date));
+        var showtimes = showtimeService.getShowtimesByDate(firstShowtimeDate);
         var moviesWithShowtimes = getMoviesWithShowtimes(showtimes);
         model.addAttribute("allDates", allDates);
         model.addAttribute("moviesWithShowtimes", moviesWithShowtimes);
         return "showtimes";
+    }
+
+    @GetMapping(params = "date")
+    public String showtimesFragmentByDate(@RequestParam("date") String date, Model model) {
+        validatePathVariable(date);
+        var showtimes = showtimeService.getShowtimesByDate(LocalDate.parse(date));
+        var moviesWithShowtimes = getMoviesWithShowtimes(showtimes);
+        model.addAttribute("moviesWithShowtimes", moviesWithShowtimes);
+        return "fragments/movie_list";
     }
 
     private Map<Movie, List<Showtime>> getMoviesWithShowtimes(List<Showtime> showtimes) {
